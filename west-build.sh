9c83326c5780f4c6b9e22ffc15d631167a21143b
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-01-04 20:29:32 +0800

    Fix failed local build due to ZMK studio flags

--- west-build.sh
+++ west-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-west build -p=always -s zmk/app -d build/left -b nice_nano_v2 -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="sofle_left" -DCONFIG_ZMK_STUDIO=y
+set -e
+
+west build -p=always -s zmk/app -d build/left -b nice_nano_v2 -S studio-rpc-usb-uart -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="sofle_left" -DCONFIG_ZMK_STUDIO=y
 cp build/left/zephyr/zmk.uf2 "./output/left.uf2"
 
-west build -p=always -s zmk/app -d build/right -b nice_nano_v2 -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="sofle_right" -DCONFIG_ZMK_STUDIO=y
+west build -p=always -s zmk/app -d build/right -b nice_nano_v2 -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="sofle_right"
 cp build/right/zephyr/zmk.uf2 "./output/right.uf2"
