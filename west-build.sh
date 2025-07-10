bd859ef752f5f19be495d65f1a0e5f643e3cc05b
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-07-11 03:14:49 +0800

    Add building of settings resetter

--- west-build.sh
+++ west-build.sh
@@ -6,3 +6,6 @@ cp build/left/zephyr/zmk.uf2 "./output/left.uf2"
 
 west build -p=always -s zmk/app -d build/right -b nice_nano_v2 -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="sofle_right"
 cp build/right/zephyr/zmk.uf2 "./output/right.uf2"
+
+west build -p=always -s zmk/app -d build/reset -b nice_nano_v2 -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="settings_reset"
+cp build/reset/zephyr/zmk.uf2 "./output/reset.uf2"
